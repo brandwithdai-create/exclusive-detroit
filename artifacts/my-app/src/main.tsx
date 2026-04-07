e4286411f2711b1130d0220aebc1394eb6c927b2
brandwithdai-create <brandwithdai@gmail.com>
2026-04-07 16:17:11 -0400

    Update main.tsx

--- artifacts/my-app/src/main.tsx
+++ artifacts/my-app/src/main.tsx
@@ -4,9 +4,3 @@ import "./index.css";
 import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(() => {});
-  });
-}
